14e9cc301beace7c32b36edd4397f96a23407b55
Alexei <alex.vyrlan@gmail.com>
2024-02-15 23:35:26 +0200

    Add new operation + *

---
+++ task2/Program.cs
@@ -0,0 +1,17 @@
+﻿int i1 = 5;
+int i2 = 6;
+int sum = i1 + i2;
+Console.WriteLine(sum);
+
+long l1 = 6;
+long sum2 = l1 + i1;
+Console.WriteLine(sum2);
+
+double d1 = 5.5;
+
+int prod1 = i1 * i2;
+long prod2 = l1 * i2;
+double prod3 = i2 * d1;
+Console.WriteLine(prod1);
+Console.WriteLine(prod2);
+Console.WriteLine(prod3);
