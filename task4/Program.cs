c46ec165c3199e2b2c6d11f9896fb520b3fb8c7c
alexidi <alex.vyrlan@gmail.com>
2024-02-16 21:11:27 +0200

    Add new folder task4 operation compare variable

---
+++ task4/Program.cs
@@ -0,0 +1,24 @@
+﻿int a = 7;
+int b = 12;
+int c = 21;
+int d = 18;
+int e = 9;
+
+int max = a;
+
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}if (d > max)
+{
+    max = d;
+}if (e > max)
+{
+    max = e;
+}
+
+Console.WriteLine(max);
